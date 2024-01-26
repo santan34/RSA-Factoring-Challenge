9625b438ff88e4e5fa56a23da1fd2f9e0a7983d8
St <takundaveni@gmail.com>
2024-01-25 19:15:56 -0800

    rsa2

---
+++ rsa.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def main():
+    import sys
+    file = sys.argv[1]
+    f = open(file, "r")
+    x = f.readline()
+    num = int(x)
+    prime = get_smallest_prime(num)
+    other = int(num/prime)
+    print(f"{num}={other}*{prime}")
+    f.close()
+
+
+def get_smallest_prime(num):
+    from math import sqrt
+    if num % 2 == 0:
+        return 2
+    for i in range(3, int(sqrt(num))+1, 2):
+        if num % i == 0:
+            return i
+
+
+if __name__ == "__main__":
+    import time
+    start = time.time()
+    main()
+    end = time.time()
+    print(end - start)
